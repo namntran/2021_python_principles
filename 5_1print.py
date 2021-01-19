07d8b8a73d19e31f015fc723524f4410c122aa9a
namntran <namtran78@gmail.com>
2021-01-19 13:48:34 +1000

    print expressions

--- 5_1print.py
+++ 5_1print.py
@@ -9,3 +9,11 @@ print(42, 4.2)  #print 2 things
 print(42,  end = ' , ')
 print(4.1, end = ' ; ')
 print(42, 4.2)
+
+#program that prints using "sep" keword parameter
+
+print(1,2,3,4,5, sep = ', ', end = '.\n')
+
+#print can be expressions
+
+print(2 * 1.00794 + 15.9994)
