f9ecfe41a326caf482c2201e514376605a815f98
namntran <namtran78@gmail.com>
2021-01-19 13:43:00 +1000

    print using end keyword parameter

--- 5_1print.py
+++ 5_1print.py
@@ -3,3 +3,9 @@ print(42)   #print an int
 print()     #print a blank line
 print(4.1)  #print a float
 print(42, 4.2)  #print 2 things
+
+#program that prints using "end" keyword paramater
+
+print(42,  end = ' , ')
+print(4.1, end = ' ; ')
+print(42, 4.2)
