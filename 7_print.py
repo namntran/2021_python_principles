af2f842be4171a30b16231a48583612c559630db
namntran <namtran78@gmail.com>
2021-01-19 14:45:50 +1000

    program that joins 2 strings

--- 7_print.py
+++ 7_print.py
@@ -4,3 +4,8 @@ H = 1.00794
 O = 15.9994
 print("molecular weight of H2O = ", H * 2 + O)
 print("molecular weight of H2O2 = ", 2 * (H+O))
+
+# program joins two strings (concatenation)
+s = "honey"
+s = s + "eater"
+print(s)
