d1d868dbdfc567c96f2ec0b5054aa008535c4297
namntran <namtran78@gmail.com>
2021-01-19 15:04:41 +1000

    program calcalute square of user input

---
+++ 8_squarenumber.py
@@ -0,0 +1,5 @@
+# print the square of a number input by the user
+
+# x = 3.0
+x = float (input ("Enter a number: ")) #convert string to float, can't multiply strings
+print("The square of the number = ", x * x)
