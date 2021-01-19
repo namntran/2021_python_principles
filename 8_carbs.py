f0ab37e69b47a010678d7be60c71a9ea3a187b7b
namntran <namtran78@gmail.com>
2021-01-19 15:14:59 +1000

    program calculates molecular weight of carbs

---
+++ 8_carbs.py
@@ -0,0 +1,11 @@
+#calculate the molecular weight of carbohydrate
+
+#define constants
+H = 1.00784
+C = 12.0096
+O = 15.99903
+
+h = int(input("How many atoms of H: "))
+c = int(input("How may atoms of C: "))
+o = int(input("How many atoms of O: "))
+print("Molecular weight =", h * H + c * C + o * O)
