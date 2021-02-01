650f1f110441086b95d1bd03ce37b3eac16ddfc4
namntran <namtran78@gmail.com>
2021-02-01 10:58:33 +1000

    print numbers from 10 down to 1 on one line

---
+++ 12_while2.py
@@ -0,0 +1,8 @@
+# while2.py
+# print numbers from 10 down to 1 on one line
+
+i = 10 #set i = 10 instead of 0
+while i >= 1: #set i= 1 instead of 10
+    print(i, end =  ", ")
+    i = i - 1
+print()
