0b7e8aba7f47a5847d25341d42e2fdeb1da52f87
namntran <namtran78@gmail.com>
2021-02-01 14:48:28 +1000

    print highest, lowest, total marks and user prompt for number of marks

---
+++ 13_marks.py
@@ -0,0 +1,22 @@
+# 13_marks.py
+# Prompt for the number of marks and read them.
+# Print the number of marks entered and the average (arithmetic mean) of marks
+# Print the  highest and lowest marks
+# use definite loop - for loop
+
+n = int(input("How many marks to be entered: "))
+mark = float(input("Enter a mark: ")) 
+highest = mark 
+lowest = mark 
+total = mark
+for i in range(n - 1):
+    mark = float(input("Enter a mark: ")) 
+    total += mark
+    if mark > highest:
+        highest = mark
+    if mark < lowest:
+        lowest = mark
+print("The number of marks: ", n)
+if n > 0: # only print average if n > 0
+    print("The average mark is: ", total/ n)
+    print("The lowest mark is: ", lowest)
