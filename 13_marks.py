3a14b32349cceb748ed7e26e2f3fd1ea09520f98
namntran <namtran78@gmail.com>
2021-02-02 13:56:00 +1000

    program that asks for marks to be entered and prints the highest, lowest, average marks

--- 13_marks.py
+++ 13_marks.py
@@ -5,18 +5,19 @@
 # use definite loop - for loop
 
 n = int(input("How many marks to be entered: "))
-mark = float(input("Enter a mark: ")) 
-highest = mark 
-lowest = mark 
-total = mark
-for i in range(n - 1):
-    mark = float(input("Enter a mark: ")) 
-    total += mark
-    if mark > highest:
-        highest = mark
-    if mark < lowest:
-        lowest = mark
-print("The number of marks: ", n)
 if n > 0: # only print average if n > 0
+    mark = float(input("Enter a mark: ")) 
+    highest = mark 
+    lowest = mark 
+    total = mark
+    for i in range(n - 1):
+        mark = float(input("Enter a mark: ")) 
+        total += mark
+        if mark > highest:
+            highest = mark
+        if mark < lowest:
+            lowest = mark
+    print("The number of marks: ", n)
     print("The average mark is: ", total/ n)
     print("The lowest mark is: ", lowest)
+    print("The highest mark is: ", highest)
