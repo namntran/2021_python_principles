cbe8e569e9e9d2113cc6dcd1640c9c12ff0800fd
namntran <namtran78@gmail.com>
2021-02-12 10:52:22 +1000

    program to calculate rugby bus capacity and hire

--- workshops/3_bus2.py
+++ workshops/3_bus2.py
@@ -1,20 +1,19 @@
 # Problem: A rugby team has 15 players. A bus company has only big buses that can carry 38 passengers.
-# Write a program that the tournament organiser can use to calculate the number  of big buses that should be hired.
+# Write a program that the tournament organiser can use to calculate the number of big buses that should be hired.
+
+# 1. ask number of teams
+# 2. calculate the number of teams and remainder (user input * 15)
+# 3. find out and  output the number of big buses
 
 t = float(15)
-b = float(38)
- 
-
-
-
+b = float(38) 
 n = int(input("How many teams? "))
 numPlayers = t * n
-print("Number of players = ", numPlayers)
+remainder = numPlayers % 38
 
-numBus = t * n / b
-print(t,n,b)
-print(numBus)
-if numBus > 1:
-    print("Number of big buses = ", numBus)
+print("Number of players: ", numPlayers)
+# print("remainder:", remainder)
+if remainder > 0:
+    print ("Number of buses to hire: ", int(numPlayers//b+1))
 else:
-    print("Number of big buses = ", numBus + 1)
+    print("Number of buses to hire: ", int(numPlayers//b))
